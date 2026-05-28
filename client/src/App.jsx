0cd6d56a1140eee5f792cf8dc2ec5d990e6f93ab
Khiem <125805195+KhiemPham16@users.noreply.github.com>
2026-05-28 23:15:05 +0700

    fix: Fix import errors in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import Header from './components/Header';
 import { Routes, Route } from 'react-router-dom';
-
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+import Blog from './pages/Blog';
 
 export default function App() {
     return (
         <>
-        <Header />
-        <Routes>
-            <Route path="/" element={<div>Home</div>} />
-            <Route path="/blog" element={<div>Blog</div>} />
-            <Route path="/contact" element={<div>Contact</div>} />
-        </Routes>
+            <Header />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/blog" element={<Blog />} />
+                <Route path="/contact" element={<Contact />} />
+            </Routes>
         </>
     );
 }
