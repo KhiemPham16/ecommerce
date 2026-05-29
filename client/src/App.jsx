4215d56fdfe1ab403a88917f4ba3cef5ec8ab94d
khiempham16 <125805195+KhiemPham16@users.noreply.github.com>
2026-05-29 22:58:44 +0700

    fix conflic fe

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,37 +3,29 @@ import Header from './components/Header';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Blog from './pages/Blog';
-import Contact from './pages/Contact'; 
+import Contact from './pages/Contact';
 import AuthLayout from './pages/Auth';
 import Login from './pages/Auth/Login';
 import Register from './pages/Auth/Register';
 import ForgotPassword from './pages/Auth/ForgotPassword';
 import ResetPassword from './pages/Auth/ResetPassword';
 import VerifyEmail from './pages/Auth/VerifyEmail';
-import Contact from './pages/Contact';
-import Blog from './pages/Blog';
 
 export default function App() {
     return (
         <>
-        <Header />
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/blog" element={<Blog />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/auth" element={<AuthLayout />}>
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="forgot-password" element={<ForgotPassword />} />
-            <Route path="reset-password" element={<ResetPassword />} />
-        </Route>
-        <Route path="/api/v1/auth/verify-email" element={<VerifyEmail />} />
-        </Routes>
             <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/blog" element={<Blog />} />
                 <Route path="/contact" element={<Contact />} />
+                <Route path="/auth" element={<AuthLayout />}>
+                    <Route path="login" element={<Login />} />
+                    <Route path="register" element={<Register />} />
+                    <Route path="forgot-password" element={<ForgotPassword />} />
+                    <Route path="reset-password" element={<ResetPassword />} />
+                </Route>
+                <Route path="/api/v1/auth/verify-email" element={<VerifyEmail />} />
             </Routes>
         </>
     );
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import { Toaster } from 'sonner';
 import { BrowserRouter as Router } from 'react-router-dom';
-import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import GolobalStyles from '~/components/GlobalStyles';
 
@@ -9,7 +8,6 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
     <>
-        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
         <Toaster richColors />
         <Router>
             <GolobalStyles>
